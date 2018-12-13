0f87060e59a2ee7c1b34b14f05449b2d4797d2d6
Pawel Chojnacki <pawel.chcki@gmail.com>
2018-12-13 15:50:46 +0100

    add zent_throw_internal

--- src/ext/dispatch.c
+++ src/ext/dispatch.c
@@ -161,6 +161,38 @@ static zend_always_inline zend_bool executing_method(zend_execute_data *execute_
 #endif
 }
 
+
+void zend_throw_exception_internal(zval *exception TSRMLS_DC) /* {{{ */
+{
+    DD_PRINTF("Hmm");
+	if (exception != NULL) {
+		zval *previous = EG(exception);
+		zend_exception_set_previous(exception, EG(exception) TSRMLS_CC);
+		EG(exception) = exception;
+		if (previous) {
+			return;
+		}
+	}
+	if (!EG(current_execute_data)) {
+		if(EG(exception)) {
+			zend_exception_error(EG(exception), E_ERROR TSRMLS_CC);
+		}
+		zend_error(E_ERROR, "Exception thrown without a stack frame");
+	}
+
+	if (zend_throw_exception_hook) {
+		zend_throw_exception_hook(exception TSRMLS_CC);
+	}
+
+	if (EG(current_execute_data)->opline == NULL ||
+	    (EG(current_execute_data)->opline+1)->opcode == ZEND_HANDLE_EXCEPTION) {
+		/* no need to rethrow the exception */
+		return;
+	}
+	EG(opline_before_exception) = EG(current_execute_data)->opline;
+	EG(current_execute_data)->opline = EG(exception_op);
+}
+
 static zend_always_inline zend_bool wrap_and_run(zend_execute_data *execute_data, zend_function *fbc,
                                                  const char *function_name, uint32_t function_name_length TSRMLS_DC) {
     zval *object = NULL;
